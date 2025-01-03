2f23460cfbb8e1ffd86179989452f5071624b7d2
Yorikas <geokaragiann@outlook.com>
2025-01-03 16:30:55 +0200

    added 3 chances functionality, made division n1/n2 n1>n2, fixed the logic of the progressive difficulty.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 float round_to_two_decimals(float value) {
-    return round(value * 100) / 100;
+    return (int)(value * 100) / 100.0;
 }
 
 const char *WRONG_RESP[] = {
@@ -56,34 +56,41 @@ int main() {
   void addition(void) {
     int correct=0;
     float ans;
-    do{
+    do {
         int n1, n2;
-        if(correct<8){
+        if(correct<8) {
             n1 = rand() % 6;
             n2 = rand() % 6;
-        }else if(correct>10){
+        } else if(correct>16) {
             n1 = rand() % 11;
             n2 = rand() % 11;
-        }else{
+        } else {
             n1 = 3 + rand() % 6;
             n2 = 3 + rand() % 6;
         }
+        int attempts = 0;
+        float correct_result = round_to_two_decimals(n1 + n2);
+        
         do {
             printf("Poso kanei: %d kai %d ?\n", n1, n2);
             scanf("%f", &ans);
 
-            float correct_result = round_to_two_decimals(n1 + n2);
             float user_answer = round_to_two_decimals(ans);
 
             if (user_answer == correct_result) {
                 printf("%s\n", RIGHT_RESP[rand() % 4]);
+                correct++;
                 break;
             } else {
+                if(attempts == 2) {
+                    printf("Prospathise na thymase oti: %d kai %d mas kanei %d\n", n1, n2, correct_result);
+                    break;
+                }
                 printf("%s\n", WRONG_RESP[rand() % 4]);
+                attempts++;
             }
-        } while (1);
-    correct++;
-  }while(correct<=15);
+        } while(1);
+    } while(correct<=24);
 }
 void subtraction(void) {
     int correct = 0;
@@ -97,7 +104,7 @@ void subtraction(void) {
                 n1 = rand() % 6;
                 n2 = rand() % 6;
             }
-        } else if(correct > 10) {
+        } else if(correct > 16) {
             n1 = rand() % 11;
             n2 = rand() % 11;
             while (n1 - n2 < 0) {
@@ -113,22 +120,29 @@ void subtraction(void) {
             }
         }
 
+        int attempts = 0;
+        float correct_result = round_to_two_decimals(n1 - n2);
+        
         do {
             printf("Poso kanei: %d plin %d ?\n", n1, n2);
             scanf("%f", &ans);
 
-            float correct_result = round_to_two_decimals(n1 - n2);
             float user_answer = round_to_two_decimals(ans);
 
             if (user_answer == correct_result) {
                 printf("%s\n", RIGHT_RESP[rand() % 4]);
+                correct++;
                 break;
             } else {
+                if(attempts == 2) {
+                    printf("Prospathise na thymase oti: %d plin %d mas kanei %d\n", n1, n2, correct_result);
+                    break;
+                }
                 printf("%s\n", WRONG_RESP[rand() % 4]);
+                attempts++;
             }
-        } while (1);
-        correct++;
-    } while(correct <= 15);
+        } while(1);
+    } while(correct <= 24);
 }
 void multiplication(void) {
     int correct = 0;
@@ -138,7 +152,7 @@ void multiplication(void) {
         if(correct < 8) {
             n1 = rand() % 6;
             n2 = rand() % 6;
-        } else if(correct > 10) {
+        } else if(correct > 16) {
             n1 = rand() % 11;
             n2 = rand() % 11;
         } else {
@@ -146,61 +160,78 @@ void multiplication(void) {
             n2 = 3 + rand() % 6;
         }
 
+        int attempts = 0;
+        float correct_result = round_to_two_decimals(n1 * n2);
+        
         do {
-            printf("Poso kanei: %d epi %d ?\n", n1, n2);
+            printf("Poso kanei: %d fores to %d ?\n", n1, n2);
             scanf("%f", &ans);
 
-            float correct_result = round_to_two_decimals(n1 * n2);
             float user_answer = round_to_two_decimals(ans);
 
             if (user_answer == correct_result) {
                 printf("%s\n", RIGHT_RESP[rand() % 4]);
+                correct++;
                 break;
-            } else {
+            }else{
+                if(attempts==2){
+                    printf("Prospathise na thymase oti: %d fores to %d mas kanei %d\n", n1, n2, correct_result);
+                    break;
+                }
                 printf("%s\n", WRONG_RESP[rand() % 4]);
-            }
-        } while (1);
-        correct++;
-    } while(correct <= 15);
+                attempts++;
+            };
+        }while(1);
+
+    } while(correct <= 24);
 }
 void division(void) {
     int correct = 0;
-    float ans;
+    int ans;
     do {
         int n1, n2;
         if(correct < 8) {
-            n1 = rand() % 6;
-            do {
-                n2 = rand() % 6;
-            } while(n2 == 0);
-        } else if(correct > 10) {
-            n1 = rand() % 11;
-            do {
-                n2 = rand() % 11;
-            } while(n2 == 0);
+            n1 = 1 + (rand() % 5);
+            n2 = 1 + (rand() % 5);
+            while(n2 > n1) {
+                n2 = 1 + (rand() % 5);
+            }
+        } else if(correct > 16) {
+            n1 = 1 + (rand() % 10);
+            n2 = 1 + (rand() % 10);
+            while(n2 > n1) {
+                n2 = 1 + (rand() % 10);
+            }
         } else {
-            n1 = 3 + rand() % 6;
-            do {
-                n2 = 3 + rand() % 6;
-            } while(n2 == 0);
+            n1 = 3 + (rand() % 6);
+            n2 = 3 + (rand() % 6);
+            while(n2 > n1) {
+                n2 = 3 + (rand() % 6);
+            }
         }
 
+        int attempts = 0;
+        int correct_result = n1 / n2;
+        
         do {
-            printf("Poso kanei: %d dia %d ?\n", n1, n2);
-            scanf("%f", &ans);
+            printf("poses fores xwraei to %d sto %d?\n", n2, n1);
+            scanf("%d", &ans);
 
-            float correct_result = round_to_two_decimals(n1 / (float)n2);
-            float user_answer = round_to_two_decimals(ans);
-
-            if (user_answer == correct_result) {
+            if (ans == correct_result) {
                 printf("%s\n", RIGHT_RESP[rand() % 4]);
+                correct++;
                 break;
             } else {
+                if(attempts == 2) {
+                    printf("Prospathise na thymase oti to %d xwraei %d fores sto %d\n", n2, correct_result, n1);
+                    break;
+                }
                 printf("%s\n", WRONG_RESP[rand() % 4]);
+                attempts++;
             }
-        } while (1);
-        correct++;
-    } while(correct <= 15);
+        } while(1);
+
+    } while(correct <= 24);
 }
 void randoperation(void){
 	int x=1+rand()%4;
